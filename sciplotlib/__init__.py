cc1f9001fac52e26b6f9e8c283ff5c3238d8922d
siaimes <34199488+siaimes@users.noreply.github.com>
2022-06-22 12:01:59 +0800

    version 0.2.0.
    Signed-off-by: siaimes <34199488+siaimes@users.noreply.github.com>

--- sciplotlib/__init__.py
+++ sciplotlib/__init__.py
@@ -6,6 +6,6 @@ A toolbox for drawing figures commonly found in journal or conference papers.
 URL: https://github.com/MetaVisionLab/SciPlotLib
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "MetaVisionLab"
 __homepage__ = "https://github.com/MetaVisionLab"
